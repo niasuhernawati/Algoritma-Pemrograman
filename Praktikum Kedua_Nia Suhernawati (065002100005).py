3e9652ae422f30e5e1e370d372f9bf7d1670b309
niasuhernawati <91404723+niasuhernawati@users.noreply.github.com>
2021-09-26 17:46:31 +0700

    Praktikum Algoritma 2

---
+++ Praktikum Kedua_Nia Suhernawati (065002100005).py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+
+@Judul : Latihan Praktikum Kedua
+@Mata Kuliah : Algoritma dan Pemrograman
+@Program Studi : Sistem Informasi
+@NIM : 065002100005
+@author: Nia Suhernawati
+
+"""
+
+print("Hitung Luas Ruang Dapur")
+
+p = float(input("berapa panjang ruang dapur?"))
+l = float(input("berapa lebar ruang dapur?"))
+n = input("masukkan satuan (Meter/Inci): ")
+L = p * l
+print("luas ruang dapur dengan panjang 15 dan lebar 8 adalah: ", str(L), "Meter")
